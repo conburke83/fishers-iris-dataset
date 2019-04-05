5e317f09ea868b11f222d592fae8ee8f361ca27e
Conor Burke <conburke@gmail.com>
2019-04-05 11:31:57 +0100

    Code written to calculate the mean of each of the first 4 columns. Next block of work - Need to elaborate on this to make the program skip over the header row in the .csv file (rather than just deleting it), and then include the header name i the print output, so the user knows what each printed mean value relates to

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -1,3 +1,19 @@
 #CB 30/03/2019
 #GMIT Data Analytics Programming & Scripting Module 2019
 #Project - Fishers Iris Dataset
+
+import numpy as np # linear algebra
+#import pandas as pd # data processing, CSV file I/O
+data = np.genfromtxt('iris.csv', delimiter = ',')
+firstcol = data[:,0]
+secondcol = data[:,1]
+thirdcol = data[:,2]
+fourthcol = data[:,3]
+meanfirstcol = np.mean(data[:,0])
+meansecondcol = np.mean(data [:,1])
+meanthirdcol = np.mean(data [:,2])
+meanfourthcol = np.mean(data [:,3])
+print ("Average of first column is: ", meanfirstcol)
+print ("Average of second column is: ", meansecondcol)
+print ("Average of third column is: ", meanthirdcol)
+print ("Average of fourth column is: ", meanfourthcol)
