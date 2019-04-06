d9706791c44345906f9fca670a03c88cea1b1a63
Conor Burke <conburke@gmail.com>
2019-04-06 10:43:07 +0100

    Extracted .csv header row into a list for use in strings throughout the program. Working to exclude the header row when performing calculations on the dataset

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -2,9 +2,19 @@
 #GMIT Data Analytics Programming & Scripting Module 2019
 #Project - Fishers Iris Dataset
 
-import numpy as np # linear algebra
-#import pandas as pd # data processing, CSV file I/O
+import numpy as np #Importing the numpy module to allow usage of inbuilt pyton functions for scientific analysis
+import csv #Importing the csv module to allow usage of inbuilt pyton functions for .csv files
+import pandas as pd # Data processing, CSV file I/O
+
+# The next section of code extracts the header names and inserts them into a list, to be used
+# later in the code. Reference 
+with open('iris.csv', 'r') as f:
+    reader = csv.reader(f)
+    i = next(reader)
+    rest = [row for row in reader]
 data = np.genfromtxt('iris.csv', delimiter = ',')
+#lines = data.readlines()[1:]
+cm = str("cm")
 firstcol = data[:,0]
 secondcol = data[:,1]
 thirdcol = data[:,2]
@@ -13,7 +23,12 @@ meanfirstcol = np.mean(data[:,0])
 meansecondcol = np.mean(data [:,1])
 meanthirdcol = np.mean(data [:,2])
 meanfourthcol = np.mean(data [:,3])
-print ("Average of first column is: ", meanfirstcol)
-print ("Average of second column is: ", meansecondcol)
-print ("Average of third column is: ", meanthirdcol)
-print ("Average of fourth column is: ", meanfourthcol)
+print ("The average",(i[0]), "is: ", meanfirstcol, cm)
+print ("The average",(i[1]), "is: ", meansecondcol, cm)
+print ("The average",(i[2]), "is: ", meanthirdcol, cm)
+print ("The average",(i[3]), "is: ", meanfourthcol, cm)
+
+#Code written to calculate the mean of each of the first 4 columns. Next block of work - Need to
+# elaborate on this to make the program skip over the header row in the .csv file (rather than just 
+# deleting it), and then include the header name i the print output, so the user knows what each
+#  printed mean value relates to"
